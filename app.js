cad1dd44dd3e70c6637236fdb233f93da80e45b3
i-Rell <rayvenblazo@gmail.com>
2025-12-30 12:42:05 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-let deferredPrompt;
 const installBtn = document.getElementById("installBtn");
+const webView = document.getElementById("webView");
+const appView = document.getElementById("appView");
 
+let deferredPrompt;
+
+/* CHECK IF INSTALLED */
+if (window.matchMedia("(display-mode: standalone)").matches) {
+    webView.hidden = true;
+    appView.hidden = false;
+} else {
+    webView.hidden = false;
+    appView.hidden = true;
+}
+
+/* INSTALL PROMPT */
 window.addEventListener("beforeinstallprompt", (e) => {
     e.preventDefault();
     deferredPrompt = e;
     installBtn.hidden = false;
 });
 
+/* INSTALL BUTTON CLICK */
 installBtn.addEventListener("click", async () => {
     installBtn.hidden = true;
     await deferredPrompt.prompt();
